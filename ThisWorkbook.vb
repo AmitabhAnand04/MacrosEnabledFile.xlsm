4b3275daab04f2c92aa3122b87b4de64bb9320a1
AmitabhAnand04 <98702241+AmitabhAnand04@users.noreply.github.com>
2024-04-12 17:37:39 +0530

    Updating Module

--- ThisWorkbook.vb
+++ ThisWorkbook.vb
@@ -1,6 +1,6 @@
 Option Explicit
 
-Private Sub Workbook_BeforeSave(ByVal SaveAsUI As Boolean, Cancel As Boolean)
-    ' Call your macro when the workbook is being saved
-    Application.Run "'Save_To_Github.xlam'!SaveToGithub.SaveVbaScriptToGitHub"
-End Sub
+'Private Sub Workbook_BeforeSave(ByVal SaveAsUI As Boolean, Cancel As Boolean)
+'    ' Call your macro when the workbook is being saved
+'    Application.Run "'Save_To_Github.xlam'!SaveToGithub.SaveVbaScriptToGitHub"
+'End Sub
