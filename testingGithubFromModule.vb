d55e1a63bb71af60fdb2d1c4c9af4c735557e097
AmitabhAnand04 <98702241+AmitabhAnand04@users.noreply.github.com>
2024-04-12 16:23:40 +0530

    Updating Module

--- testingGithubFromModule.vb
+++ testingGithubFromModule.vb
@@ -1,5 +1,5 @@
 Option Explicit
 
-Sub testing()
-    Application.Run "'Save_To_Github.xlam'!SaveToGithub.SaveVbaScriptToGitHub"
-End Sub
+'Sub testing()
+'    Application.Run "'Save_To_Github.xlam'!SaveToGithub.SaveVbaScriptToGitHub"
+'End Sub
